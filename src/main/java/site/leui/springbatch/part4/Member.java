656ea1fa34d809d8969bc983d41df710c9a00bea
Eui9179 <outdark97@gmail.com>
2023-06-04 19:43:50 +0900

    delete-of

--- src/main/java/site/leui/springbatch/part4/Member.java
+++ src/main/java/site/leui/springbatch/part4/Member.java
@@ -8,19 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
+@NoArgsConstructor
 @Entity
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    public static Member of(String name) {
-        return Member.builder()
-                .name(name)
-                .build();
-    }
 }
